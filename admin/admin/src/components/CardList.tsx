56ce1d872167236373f3c619f28f15a32fee3fec
masumali23020 <mdmasum0218@gmail.com>
2025-11-24 21:05:07 +0600

    admin

---
+++ admin/admin/src/components/CardList.tsx
@@ -0,0 +1,123 @@
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { Card, CardContent, CardFooter, CardTitle } from "./ui/card";
+
+const popularContent = [
+  {
+    id: 1,
+    name: "Adidas CoreFit T-Shirt",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 39.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "purple", "green"],
+    images: {
+      gray: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=800",
+      purple: "/products/1p.png",
+      green: "/products/1gr.png",
+    },
+  },
+];
+
+const latestTransactions = [
+  {
+    id: 1,
+    title: "Oder Payment",
+    badge: "John Doe",
+    image:
+      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1400,
+  },
+  {
+    id: 2,
+    title: "Oder Payment",
+    badge: "Jane Smith",
+    image:
+      "https://images.pexels.com/photos/4969918/pexels-photo-4969918.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 2100,
+  },
+  {
+    id: 3,
+    title: "Oder Payment",
+    badge: "Michael Johnson",
+    image:
+      "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1300,
+  },
+  {
+    id: 4,
+    title: "Oder Payment",
+    badge: "Lily Adams",
+    image:
+      "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 2500,
+  },
+  {
+    id: 5,
+    title: "Subscription Renewal",
+    badge: "Sam Brown",
+    image:
+      "https://images.pexels.com/photos/1680175/pexels-photo-1680175.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1400,
+  },
+];
+
+const CardList = ({ title }: { title: string }) => {
+  return (
+    <div className="">
+      <h1 className="text-lg font-medium mb-6">{title}</h1>
+      <div className="flex flex-col gap-2">
+        {title === "Popular Products"
+          ? popularContent.map((item) => (
+              <Card
+                key={item.id}
+                className="flex-row items-center justify-between gap-4 p-4"
+              >
+                <div className="w-12 h-12 rounded-sm relative overflow-hidden">
+                  <Image
+                    src={Object.values(item.images)[0] || ""}
+                    alt={item.name}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <CardContent className="flex-1 p-0">
+                  <CardTitle className="text-sm font-medium">
+                    {item.name}
+                  </CardTitle>
+                </CardContent>
+                <CardFooter className="p-0">${item.price / 1000}K</CardFooter>
+              </Card>
+            ))
+          : latestTransactions.map((item) => (
+              <Card
+                key={item.id}
+                className="flex-row items-center justify-between gap-4 p-4"
+              >
+                <div className="w-12 h-12 rounded-sm relative overflow-hidden">
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <CardContent className="flex-1 p-0">
+                  <CardTitle className="text-sm font-medium">
+                    {item.title}
+                  </CardTitle>
+                  <Badge variant="outline" className="mt-1">
+                    {item.badge}
+                  </Badge>
+                </CardContent>
+                <CardFooter className="p-0">${item.count / 1000}K</CardFooter>
+              </Card>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
